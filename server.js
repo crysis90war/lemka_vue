256d2c377abb5913904e6e71a3b8d5f6be5b8428
Samad Hatsijev <crysis90war@hotmail.be>
2021-03-14 13:13:07 +0100

    trying to deploy dist now

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const serverStatic = require('serve-statice')
+const serverStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
