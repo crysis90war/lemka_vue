af937aaae9eea95abb51d3b40b7118f0b9d5781d
Samad Hatsijev <crysis90war@hotmail.be>
2021-03-14 13:27:54 +0100

    fixing bugs for deployment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const serverStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 
-const app = express()
+const app = express();
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+app.use(serveStatic(path.join(path.resolve(), 'dist')));
 
-const port = process.env.PORT || 8080
-app.listen(port)
-
-console.log('Listening on port: ' + port)
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
+});
