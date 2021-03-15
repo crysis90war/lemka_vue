6e993201cffa9370108dd0bbbc418441985a453c
Samad Hatsijev <crysis90war@hotmail.be>
2021-03-15 23:47:31 +0100

    Test router in prod
    Trying to fix history in vue-router
    - add history in server.js
    
    ! testing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const history = require('connect-history-api-fallback')
 const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
+
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
 
 app.use(serveStatic(path.join(path.resolve(), 'dist')));
 
