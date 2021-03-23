00248971281781e5dd06858889a87d5d770e87c2
Samad Hatsijev <crysis90war@hotmail.be>
2021-03-23 22:20:01 +0100

    carousel fixed & build

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,17 +2,21 @@
   <div class="lemka_carousel">
     <l-carousel :controls-visible="true" :clickable="true" :border="0" :space="680"
                 :width="680" :height="480" :perspective="25"
-                :inverseScaling="150" :display="3" :loop="true" :autoplay="true" :autoplayTimeout="5000" :animationSpeed="2000">
+                :inverseScaling="150" :display="3" :loop="true" :autoplay="true" :autoplayTimeout="15000" :animationSpeed="3000">
       <l-slide v-for="(slide, i) in slides" :key="i" :index="i">
-        <figure :style="`background-image: url(${slide}); background-repeat: no-repeat; background-size: cover;`" class="h-100 w-100">
-          <b-container class="h-100">
-            <b-card class="text-center" title="Card Title" tag="article" style="max-width: 20rem; top: 25%; left: 25%; display: none;">
-              <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-              </b-card-text>
-              <b-link href="#" class="text-secondary">Voir plus</b-link>
-            </b-card>
-          </b-container>
+        <figure :style="`background-image: url(${slide}); background-repeat: no-repeat; background-size: cover;`"
+                class="h-100 w-100 text-center">
+
+          <figcaption class="fig-top" style="display: none;">
+            <h4>Title</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </figcaption>
+
+          <figcaption class="fig-bottom" style="display: none;">
+            <b-button variant="light" disabled>
+              Voir plus
+            </b-button>
+          </figcaption>
         </figure>
       </l-slide>
     </l-carousel>
@@ -41,10 +45,24 @@ export default {
 
 .carousel-3d-slide.current {
   > figure {
-    > .container {
-      > .card {
-        display: block !important;
-      }
+    > .fig-top {
+      display: block !important;
+      position: absolute;
+      background-color: #FFFFFF;
+      opacity: 0.7;
+      color: #000000;
+      top: 0;
+      padding: 10px;
+      min-width: 100%;
+      box-sizing: border-box;
+    }
+    > .fig-bottom {
+      display: block !important;
+      position: absolute;
+      bottom: 0;
+      padding: 15px;
+      min-width: 100%;
+      box-sizing: border-box;
     }
   }
 }
