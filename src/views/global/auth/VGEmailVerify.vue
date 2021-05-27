837fbd52f13a9bc507188f3082a830a3c607db7a
Samad Hatsijev <crysis90war@hotmail.be>
2021-05-27 21:22:21 +0200

    feat: verify email redirect

--- src/views/global/auth/VGEmailVerify.vue
+++ src/views/global/auth/VGEmailVerify.vue
@@ -2,16 +2,17 @@
   <div class="email_verify">
     <div class="d-flex justify-content-center">
       <div class="text-center my-5">
-        <div>
-          <b-alert variant="success" show="">Votre compte a été activé avec succès</b-alert>
-          <router-link to="">Se connecter</router-link>
+        <h3>Activation du compte</h3>
+        <div v-if="$route.query.token_valid === 'True'">
+          <b-alert variant="success" show="">{{ $route.query.message }}</b-alert>
+          <router-link :to="{name: 'VGLogin'}">Se connecter</router-link>
         </div>
-        <div>
-          <b-alert variant="danger" show="">Une erreur s'est produite lors d'activation</b-alert>
+        <div v-else>
+          <b-alert variant="danger" show="">{{ $route.query.message }}</b-alert>
         </div>
       </div>
     </div>
-    <pre style="white-space: pre-wrap">{{$route.query}}</pre>
+<!--    <pre style="white-space: pre-wrap">{{$route.query}}</pre>-->
   </div>
 </template>
 
