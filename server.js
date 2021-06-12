135de68520580d92fc99c03546dff52220f3c493
Samad Hatsijev <crysis90war@hotmail.be>
2021-06-12 16:52:13 +0200

    fix: sslredirect

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const history = require('connect-history-api-fallback')
 const path = require('path');
 const serveStatic = require('serve-static');
-const sslRedirect = require('heroku-ssl-redirect')
 
 const app = express();
 
@@ -19,8 +18,6 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log('Listening on port: ' + port);
 });
-
-app.use(sslRedirect());
 
 // /* Redirect http to https */
 // app.get("*", function (req, res, next) {
