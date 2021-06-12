7d7dba2363c7f4216f5c0ece0ac4d1e7635b31c5
Samad Hatsijev <crysis90war@hotmail.be>
2021-06-12 22:17:03 +0200

    fix: fb logout

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import authHeader from "@/services/auth-header";
+import jwt_decode from "jwt-decode";
 
 const DOMAIN = process.env.VUE_APP_NODE_END === 'development' ? 'http://127.0.0.1:8000' : 'https://lemka-api.herokuapp.com'
 
@@ -59,6 +60,13 @@ class ApiService {
 
     DESTROYSession() {
         // sessionStorage.removeItem('user');
+        let user = JSON.parse(localStorage.getItem('user'));
+        let token = jwt_decode(user.access)
+        if (token.auth_provider === 'facebook') {
+            // eslint-disable-next-line no-undef
+            FB.api('/me/permissions', 'delete', null, () => FB.logout());
+        }
+
         localStorage.removeItem('user');
     }
 }
