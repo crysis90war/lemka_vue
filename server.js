dab544328ec7017e3a5fbdf6cd3724d5bb1d19a9
Samad Hatsijev <crysis90war@hotmail.be>
2021-06-12 17:04:16 +0200

    fix: sslredirect

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const sslRedirect = require('heroku-ssl-redirect').default
 const express = require('express');
 const history = require('connect-history-api-fallback')
 const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
+
+app.use(sslRedirect())
+// /* Redirect http to https */
+// app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https') {
+//         res.redirect(`https://${req.header('host')}${req.url}`)
+//     } else {
+//         next();
+//     }
+// });
 
 // Use a fallback for non-root routes (required for Vue router)
 //   NOTE: History fallback must be "used" before the static serving middleware!
@@ -19,14 +30,6 @@ app.listen(port, () => {
     console.log('Listening on port: ' + port);
 });
 
-// /* Redirect http to https */
-app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-        res.redirect(`https://${req.header('host')}${req.url}`)
-    } else {
-        next();
-    }
-});
 // app.get("*", function (req, res, next) {
 //     if ("https" !== req.headers["x-forwarded-proto"] && "production" === process.env.NODE_ENV) {
 //         res.redirect("https://" + req.hostname + req.url);
