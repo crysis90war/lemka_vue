04fc468c20d3ccc25971c8d1760ce1cdcfa927a7
Samad Hatsijev <crysis90war@hotmail.be>
2021-06-12 21:03:13 +0200

    sslredirect

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const sslRedirect = require('heroku-ssl-redirect').default
+// const sslRedirect = require('heroku-ssl-redirect').default
 const express = require('express');
 const history = require('connect-history-api-fallback')
 const path = require('path');
@@ -6,15 +6,15 @@ const serveStatic = require('serve-static');
 
 const app = express();
 
-app.use(sslRedirect())
-// /* Redirect http to https */
-// app.use((req, res, next) => {
-//     if (req.header('x-forwarded-proto') !== 'https') {
-//         res.redirect(`https://${req.header('host')}${req.url}`)
-//     } else {
-//         next();
-//     }
-// });
+// app.use(sslRedirect())
+/* Redirect http to https */
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+        next();
+    }
+});
 
 // Use a fallback for non-root routes (required for Vue router)
 //   NOTE: History fallback must be "used" before the static serving middleware!
