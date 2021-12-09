634ebfdf2ad3c94c64404989c0f9c51e11c544d7
Samad Hatsijev <s.hatsijev@hotmail.com>
2021-12-09 14:47:15 +0100

    test config vetur

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
-// const sslRedirect = require('heroku-ssl-redirect').default
-const express = require('express');
-const history = require('connect-history-api-fallback')
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require("express");
+const history = require("connect-history-api-fallback");
+const path = require("path");
+const serveStatic = require("serve-static");
 
 const app = express();
 
-// app.use(sslRedirect())
-/* Redirect http to https */
 app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-        res.redirect(`https://${req.header('host')}${req.url}`)
+    if (req.header("x-forwarded-proto") !== "https") {
+        res.redirect(`https://${req.header("host")}${req.url}`);
     } else {
         next();
     }
 });
 
-// Use a fallback for non-root routes (required for Vue router)
-//   NOTE: History fallback must be "used" before the static serving middleware!
-app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    verbose: true
-}))
+app.use(
+    history({
+        verbose: true,
+    })
+);
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, "dist")));
 
 const port = process.env.PORT || 80;
 app.listen(port, () => {
-    console.log('Listening on port: ' + port);
+    console.log("Listening on port: " + port);
 });
 
+// const sslRedirect = require('heroku-ssl-redirect').default
+// app.use(sslRedirect())
 // app.get("*", function (req, res, next) {
 //     if ("https" !== req.headers["x-forwarded-proto"] && "production" === process.env.NODE_ENV) {
 //         res.redirect("https://" + req.hostname + req.url);
--- src/views/utilisateur/demande_de_devis/VUDemandeDevis.vue
+++ src/views/utilisateur/demande_de_devis/VUDemandeDevis.vue
@@ -1,35 +1,35 @@
 <template>
-  <section v-if="$route.name === routes.DEMANDE_DE_DEVIS.name" class="demande_devis">
+  <section
+    v-if="$route.name === routes.DEMANDE_DE_DEVIS.name"
+    class="demande_devis"
+  >
     <b-card
-        title="Demande de devis"
-        :class="BSClass.CARD_BORDERLESS_SHADOW" class="my-4"
+      title="Demande de devis"
+      :class="BSClass.CARD_BORDERLESS_SHADOW"
+      class="my-4"
     >
       <b-card-body>
         <l-create-refresh
-            size="sm"
-            :route="routes.DEMANDE_DE_DEVIS_ADD_OR_UPDATE.name"
-            :load-or-refresh="loadOrRefresh"
+          size="sm"
+          :route="routes.DEMANDE_DE_DEVIS_ADD_OR_UPDATE.name"
+          :load-or-refresh="loadOrRefresh"
         />
 
-        <b-tabs
-            pills
-            card
-            class="mt-2"
-        >
+        <b-tabs pills card class="mt-2">
           <b-tab title="En rédaction">
             <b-card-text>
               <b-table
-                  :items="demandesDevisEnRedaction"
-                  :fields="redactionTableFields"
-                  :busy="busy"
-                  stacked="md"
-                  small
-                  show-empty
-                  hover
-                  class="text-center mt-3"
+                :items="demandesDevisEnRedaction"
+                :fields="redactionTableFields"
+                :busy="busy"
+                stacked="md"
+                small
+                show-empty
+                hover
+                class="text-center mt-3"
               >
                 <template #table-busy>
-                  <l-table-busy/>
+                  <l-table-busy />
                 </template>
 
                 <template #cell(created_at)="data">
@@ -38,24 +38,33 @@
 
                 <template #cell(est_urgent)="data">
                   <b-badge
-                      pill
-                      :variant="data.item.est_urgent === true ? 'success' : 'danger'"
+                    pill
+                    :variant="
+                      data.item.est_urgent === true ? 'success' : 'danger'
+                    "
                   >
-                    <i :class="`fas fa-${data.item.est_urgent  === true ? 'check' : 'times'}-circle`"></i>
+                    <i
+                      :class="`fas fa-${
+                        data.item.est_urgent === true ? 'check' : 'times'
+                      }-circle`"
+                    ></i>
                   </b-badge>
                 </template>
 
                 <template #cell(actions)="data">
                   <b-button-group size="sm">
                     <b-button
-                        variant="outline-primary"
-                        :to="{name: routes.DEMANDE_DE_DEVIS_ADD_OR_UPDATE.name, params: {id: data.item.id}}"
+                      variant="outline-primary"
+                      :to="{
+                        name: routes.DEMANDE_DE_DEVIS_ADD_OR_UPDATE.name,
+                        params: { id: data.item.id },
+                      }"
                     >
                       <i class="fas fa-edit"></i>
                     </b-button>
                     <b-button
-                        variant="outline-success"
-                        @click.stop.prevent="envoyer(data.item)"
+                      variant="outline-success"
+                      @click.stop.prevent="envoyer(data.item)"
                     >
                       <i class="fas fa-paper-plane"></i>
                     </b-button>
@@ -68,39 +77,42 @@
           <b-tab title="Soumis">
             <b-card-text>
               <b-table
-                  :items="demandeDevisSoumis"
-                  :fields="soumisTableFields"
-                  :busy="busy"
-                  stacked="md"
-                  small
-                  show-empty
-                  hover
-                  class="text-center mt-3"
+                :items="demandeDevisSoumis"
+                :fields="soumisTableFields"
+                :busy="busy"
+                stacked="md"
+                small
+                show-empty
+                hover
+                class="text-center mt-3"
               >
                 <template #table-busy>
-                  <l-table-busy/>
+                  <l-table-busy />
                 </template>
 
                 <template #empty>
-                  <l-table-empty/>
+                  <l-table-empty />
                 </template>
                 <template #cell(created_at)="data">
                   {{ data.item.created_at | localTimeStr }}
                 </template>
                 <template #cell(est_urgent)="data">
                   <b-badge
-                      :variant="data.item.est_urgent === true ? 'success' : 'danger'"
-                      pill
+                    :variant="
+                      data.item.est_urgent === true ? 'success' : 'danger'
+                    "
+                    pill
                   >
-                    <i :class="`fas fa-${data.item.est_urgent === true ? 'check' : 'times'}-circle`"></i>
+                    <i
+                      :class="`fas fa-${
+                        data.item.est_urgent === true ? 'check' : 'times'
+                      }-circle`"
+                    ></i>
                   </b-badge>
                 </template>
 
                 <template #cell(actions)>
-                  <b-button
-                      size="sm"
-                      variant="outline-primary"
-                  >
+                  <b-button size="sm" variant="outline-primary">
                     <i class="fas fa-eye"></i>
                   </b-button>
                 </template>
@@ -109,28 +121,30 @@
           </b-tab>
 
           <b-tab title="En cours">
-            <b-card-text>Vos demandes de devis en cours de traitements.</b-card-text>
+            <b-card-text
+              >Vos demandes de devis en cours de traitements.</b-card-text
+            >
             <b-table
-                :items="demandeDevisEnCours"
-                :fields="traiteTableFields"
-                show-empty
+              :items="demandeDevisEnCours"
+              :fields="traiteTableFields"
+              show-empty
             ></b-table>
           </b-tab>
 
           <b-tab title="Traités">
             <b-card-text>
               <b-table
-                  :items="demandeDevisTraite"
-                  :fields="traiteTableFields"
-                  :busy="busy"
-                  stacked="md"
-                  small
-                  show-empty
-                  hover
-                  class="text-center mt-3"
+                :items="demandeDevisTraite"
+                :fields="traiteTableFields"
+                :busy="busy"
+                stacked="md"
+                small
+                show-empty
+                hover
+                class="text-center mt-3"
               >
                 <template #table-busy>
-                  <l-table-busy/>
+                  <l-table-busy />
                 </template>
                 <template #cell(created_at)="data">
                   {{ data.item.created_at | localTimeStr }}
@@ -140,25 +154,24 @@
           </b-tab>
         </b-tabs>
       </b-card-body>
-
     </b-card>
   </section>
 
-  <router-view v-else/>
+  <router-view v-else />
 </template>
 
 <script>
 import LemkaHelpers from "@/helpers";
-import {mapActions, mapGetters} from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import DemandeDevisModel from "@/models/devis/demande_devis.model";
-import {localTimeStr} from "@/utils/filters";
-import {htmlTitle} from "@/utils/tools";
+import { localTimeStr } from "@/utils/filters";
+import { htmlTitle } from "@/utils/tools";
 
 export default {
   name: "VUDemandeDevis",
   props: {},
   title() {
-    return htmlTitle("Demande de devis")
+    return htmlTitle("Demande de devis");
   },
   data() {
     return {
@@ -166,7 +179,7 @@ export default {
       routes: LemkaHelpers.Routes,
       redactionTableFields: DemandeDevisModel.redactionTableFields,
       traiteTableFields: DemandeDevisModel.traiteTableFields,
-    }
+    };
   },
   computed: {
     ...mapGetters({
@@ -178,35 +191,37 @@ export default {
       demandeDevisTraite: "DemandesDevis/demandeDevisTraite",
     }),
     soumisTableFields() {
-      return DemandeDevisModel.soumisTableFields
-    }
+      return DemandeDevisModel.soumisTableFields;
+    },
   },
   methods: {
-    ...mapActions({loadDemandeDevis: "DemandesDevis/loadDemandeDevis", updateDemandeDevis: "DemandesDevis/updateDemandeDevis"}),
-    loadOrRefresh: async function() {
-      await this.loadDemandeDevis()
+    ...mapActions({
+      loadDemandeDevis: "DemandesDevis/loadDemandeDevis",
+      updateDemandeDevis: "DemandesDevis/updateDemandeDevis",
+    }),
+    loadOrRefresh: async function () {
+      await this.loadDemandeDevis();
     },
     envoyer: function (item) {
-      let demande_devis = new DemandeDevisModel(item)
-      let payload = demande_devis.toUpdatePayload()
-      payload.est_soumis = true
-      this.updateDemandeDevis(payload)
+      let demande_devis = new DemandeDevisModel(item);
+      let payload = demande_devis.toUpdatePayload();
+      payload.est_soumis = true;
+      this.updateDemandeDevis(payload);
     },
   },
   created() {
     if (this.demandes_devis.length === 0) {
-      this.loadOrRefresh()
+      this.loadOrRefresh();
     }
   },
   filters: {
     localTimeStr: function (value) {
-      value = localTimeStr(value)
-      return value
-    }
-  }
-}
+      value = localTimeStr(value);
+      return value;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
